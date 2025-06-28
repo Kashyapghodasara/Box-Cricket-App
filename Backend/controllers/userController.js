c14f01805dd5ed615880ef8473a897f89912c306
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-06-28 11:01:51 +0530

    Clean rollback: remove .env, add .env.sample, fix .gitignore and config files

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+import DBConnection from "../config/database"
+import User from "../models/userSchema.js"
+DBConnection()
+
+// Start creating Signup and Login and logout
+// Tested on postman
+// Then learn Superset + MMS + Jest
--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,12 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import DBConnection from './config/database.js';
+import userRouter from './routes/userRoute.js'
 
 
 const app = express()
+DBConnection()
 dotenv.config({path: ".env"})
 
 const __filename = fileURLToPath(import.meta.url);  // You have to import this first
@@ -22,6 +25,7 @@ app.use(cors({
     credentials: true
 }))
 
+app.use("/api/v1/user", userRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT} ⚙`)
