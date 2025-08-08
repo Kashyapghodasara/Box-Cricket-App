37c7579de9aa090c294a4d3d7027f01495f3bd72
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-08-08 15:07:58 +0530

    Ignore backend and build caches in admin branch

--- Backend/routes/adminRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { adminLogin } from "../controllers/adminController.js"
-
-const router = express.Router()
-
-router.route('/adminLogin').post(adminLogin)
-
-export default router
