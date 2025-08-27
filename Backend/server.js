cf5dd868668872cd554d9643356bed5b183c0d23
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-08-27 17:57:40 +0530

    Try to solve the error

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import helmet from "helmet";
 import { rateLimit } from 'express-rate-limit';
+import axios from 'axios';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -15,6 +16,7 @@ import adminRouter from './routes/adminRoute.js';
 export const app = express()
 DBConnection()
 dotenv.config({ path: ".env" })
+axios.defaults.withCredentials = true;
 
 const __filename = fileURLToPath(import.meta.url);  // You have to import this first
 const __dirname = path.dirname(__filename);
