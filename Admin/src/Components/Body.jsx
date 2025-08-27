4239678bbd143a8f3b532979da48304fb0a427aa
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-08-27 14:43:05 +0530

    bug fixing for Admin

--- Admin/src/Components/Body.jsx
+++ Admin/src/Components/Body.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import Home from "./Home";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import RevenuePage from "./Revenue/RevenuePage";
 import TransDetails from "./Transaction/TransDetails";
-import Sidebar from "./Sidebar.jsx";
 
 const Body = () => {
-
-    const appRouter = createBrowserRouter([
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate to="/Overview" replace />,
+    },
+    {
+      path: "/Overview",
+      element: <Home />,
+      children: [
         {
-            path: "/Overview",
-            element: <Home />,
-            children: [{
-                path: "/Overview",
-                element: <Dashboard />
-            }]
+          index: true,
+          element: <Dashboard />,
         },
-        {
-            path: '/Revenue',
-            element: <RevenuePage />
-        }, 
-        {
-            path: '/Transaction',
-            element: <TransDetails />,
-        }
-    ])
+      ],
+    },
+    {
+      path: "/Revenue",
+      element: <RevenuePage />,
+    },
+    {
+      path: "/Transaction",
+      element: <TransDetails />,
+    },
+  ]);
 
-    return (
-        <RouterProvider router={appRouter} />
-    );
-}
+  return <RouterProvider router={appRouter} />;
+};
 
-export default Body
-
+export default Body;
