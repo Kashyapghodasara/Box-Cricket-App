8667e36c78c26752e093799ce0abf9ea35e15e1e
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-08-30 14:25:19 +0530

    Admin Login Controller fixed

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -90,7 +90,7 @@ export const adminLogin = async (req, res) => {
         return res.cookie("adminToken", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "None",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         }).status(200).json({
             message: "Admin logged in successfully",
